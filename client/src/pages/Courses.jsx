867eb210aaa4e4dc3741aaf0d936fcc85aee6f04
Nitin Kumar <np747560@gmail.com>
2026-08-23 22:29:15 +0530

    Connect courses to production API

--- client/src/pages/Courses.jsx
+++ client/src/pages/Courses.jsx
@@ -19,7 +19,7 @@ const Courses = () => {
   useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await fetch(
+         const response = await fetch(
   "https://genlearning-production.up.railway.app/api/courses"
 );
 
